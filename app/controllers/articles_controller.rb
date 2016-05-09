6cd392ff0a402bf0eef3fd9267e2cb8c5f9e66b5
Ziv Zamechek <ziv921@yahoo.com>
2016-05-09 11:38:45 -0400

    editted the layout page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,16 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def new
     @article = Article.new
-  end 
+  end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -23,7 +24,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -33,10 +33,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
