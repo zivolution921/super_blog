58cde1a5d4074bfc4973283b716bd9361ec9cc16
Ziv Zamechek <ziv921@yahoo.com>
2016-05-09 15:11:24 -0400

    add secure pass and attributes to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   
   has_many :articles
+  has_secure_password
   before_save {self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
