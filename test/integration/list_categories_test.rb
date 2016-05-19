5e3ecdbc6af65ac3f455c817989996a127298ff4
Ziv Zamechek <ziv921@yahoo.com>
2016-05-19 09:56:22 -0400

    fixed list categories test bug

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @category = Category.create(name: "books")
-    @category = Category.create(name: "programming")
+    @category = Category.create(name: "sports")
+    @category2 = Category.create(name: "programming")
   end
 
   test "should show categories listing" do 
