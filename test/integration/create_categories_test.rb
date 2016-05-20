0df2fa310d19fe7a09def7849b49f3621962ff01
Ziv Zamechek <ziv921@yahoo.com>
2016-05-20 16:36:26 -0400

    Fixed tests and updated navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+
 
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do 
@@ -14,7 +19,8 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match "sports", response.body
   end
 
-  test "invalid category submission results in failure" do 
+  test "invalid category submission results in failure" do
+    sign_in_as(@user, "password") 
     get new_category_path
     assert_template 'categories/new'
     #difference in category
